c209e23c4ae33099c729de00cf9e6b4039b0987d
AlexDev <ariverar151097@icloud.com>
2025-05-26 19:21:38 -0600

    userRepoFt, add findUserByEmail

--- vetApp/src/main/java/com/backend/vetApp/Repository/User/UserRepository.java
+++ vetApp/src/main/java/com/backend/vetApp/Repository/User/UserRepository.java
@@ -3,6 +3,9 @@ package com.backend.vetApp.Repository.User;
 import com.backend.vetApp.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmailAndPassword(String email, String password);
+    Optional<User> findByEmail(String email);
 }
