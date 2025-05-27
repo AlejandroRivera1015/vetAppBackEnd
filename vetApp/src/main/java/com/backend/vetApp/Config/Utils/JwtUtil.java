a1ca628dc9268177c5421f60d1bf2bd811acf245
AlexDev <ariverar151097@icloud.com>
2025-05-26 23:01:06 -0600

    Jwt class for generate / validate jwtoken

---
+++ vetApp/src/main/java/com/backend/vetApp/Config/Utils/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.backend.vetApp.Config.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Map;
+
+@Getter
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public  String generateToken(Map<String,String> claims){
+        byte[] decodedKey = Base64.getDecoder().decode(getSecret());
+        SecretKey key = Keys.hmacShaKeyFor(decodedKey);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(claims.get("id"))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+
+    public Jws<Claims> validateToken(String token) {
+        try {
+            return  Jwts.parserBuilder().setSigningKey(getSecret()).build().parseClaimsJws(token);
+        }catch (Exception e) {
+            throw new RuntimeException("Invalid JWT token", e);
+        }
+
+    }
+}
