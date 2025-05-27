a005f56bd457888ab95c7de1120d59afff429132
AlexDev <ariverar151097@icloud.com>
2025-05-27 13:08:36 -0600

    add new constructor to create exception with user email

--- vetApp/src/main/java/com/backend/vetApp/Exception/User/UserException.java
+++ vetApp/src/main/java/com/backend/vetApp/Exception/User/UserException.java
@@ -1,10 +1,18 @@
 package com.backend.vetApp.Exception.User;
 
 public class UserException extends  Exception {
-   private Long userId;
+    private Long userId;
+    private String userEmail;
 
-   public  UserException(String message, Long userId){
-         super(message);
-         this.userId = userId;
-   }
+
+    public UserException(String message, Long userId) {
+        super(message);
+        this.userId = userId;
+    }
+
+    public UserException(String message, String email) {
+        super(message);
+        this.userEmail = email;
+
+    }
 }
