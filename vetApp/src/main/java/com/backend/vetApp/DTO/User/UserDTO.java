540eaff7990b071c9634798bde4d3c8017d07c62
AlexDev <ariverar151097@icloud.com>
2025-05-27 22:09:19 -0600

    fix UserDTO class location

--- vetApp/src/main/java/com/backend/vetApp/DTO/User/UserDTO.java
+++ vetApp/src/main/java/com/backend/vetApp/DTO/User/UserDTO.java
@@ -1,13 +1,14 @@
-package com.backend.vetApp.DTO.UserDTO;
-
+package com.backend.vetApp.DTO.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class UserDTO {
     private  String email;
     private  String password;
