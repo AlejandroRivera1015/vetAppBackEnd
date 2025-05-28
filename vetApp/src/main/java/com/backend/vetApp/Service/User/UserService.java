f92265cd71c8b2b52983f19e6fddf0606b3c35ad
AlexDev <ariverar151097@icloud.com>
2025-05-28 00:01:42 -0600

    User service for handleLogin by admin, receptionist, doctor, and client

--- vetApp/src/main/java/com/backend/vetApp/Service/User/UserService.java
+++ vetApp/src/main/java/com/backend/vetApp/Service/User/UserService.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UserService {
     Boolean createUser(UserDTO user);
+    Boolean logIn(UserDTO user);
 }
--- vetApp/src/main/java/com/backend/vetApp/Service/User/UserServiceImpl.java
+++ vetApp/src/main/java/com/backend/vetApp/Service/User/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.backend.vetApp.Service.User;
 
 import com.backend.vetApp.DTO.User.UserDTO;
+import com.backend.vetApp.Entity.Admin.Admin;
 import com.backend.vetApp.Entity.Client.Client;
+import com.backend.vetApp.Entity.HealtPersonal.Dr;
+import com.backend.vetApp.Exception.User.UserException;
+import com.backend.vetApp.Repository.Admin.AdminRepository;
 import com.backend.vetApp.Repository.Client.ClientRepository;
 import com.backend.vetApp.Repository.Doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,8 @@ public class UserServiceImpl implements UserService {
     ClientRepository clientRepository;
     @Autowired
     DoctorRepository doctorRepository;
+    @Autowired
+    AdminRepository adminRepository;
 
     @Override
     public  Boolean createUser(UserDTO userDTO) {
@@ -50,5 +56,53 @@ public class UserServiceImpl implements UserService {
 
         return  false;
 
+    }
+
+    @Override
+    public Boolean logIn(UserDTO  userDTO){
+        String encryptedPassword = EncryptDataUtil.toEncrypt(userDTO.getPassword());
+        System.out.println("Encrypted Password: " + encryptedPassword);
+        try{
+            switch (userDTO.getRole()) {
+                case "admin", "receptionist" -> {
+                    Optional<Admin> admin = adminRepository.findByEmail(userDTO.getEmail());
+                    if(admin.isPresent()){
+                        return EncryptDataUtil.toDecrypt(userDTO.getPassword(),admin.get().getPassword());
+                    }
+                    else  {
+                        throw new UserException("UserNotFound", userDTO.getEmail());
+                    }
+
+                }
+
+                case "doctor" ->{
+                    Optional<Dr> doctor = doctorRepository.findByEmail(userDTO.getEmail());
+                    if ((doctor.isPresent())){
+                        return  EncryptDataUtil.toDecrypt(userDTO.getEmail(), doctor.get().getPassword());
+                    }
+                    else {
+                        throw new UserException("UserNotFound", userDTO.getEmail());
+                    }
+
+                }
+
+                case "client" ->{
+                    Optional<Client> client = clientRepository.findByEmail(userDTO.getEmail());
+                    if(client.isPresent()){
+                        return EncryptDataUtil.toDecrypt(userDTO.getPassword(), client.get().getPassword());
+                    }
+                    else {
+                        throw new UserException("UserNotFound", userDTO.getEmail());
+                    }
+
+                }
+
+                default -> {
+                    throw new UserException("UserNotFound", userDTO.getEmail());
+                }
+            }
+        }catch (UserException e){
+            return  false;
+        }
     }
 }
