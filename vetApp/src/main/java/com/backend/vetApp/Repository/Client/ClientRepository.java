c4adf57c7150bafefa3b14d3f881827953ae8379
AlexDev <ariverar151097@icloud.com>
2025-05-28 00:03:11 -0600

    Update user Repo's with findByEmail method

--- vetApp/src/main/java/com/backend/vetApp/Repository/Client/ClientRepository.java
+++ vetApp/src/main/java/com/backend/vetApp/Repository/Client/ClientRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    Optional<Client> findByEmail(String email);
     Optional<Client> findByEmailAndPassword(String email, String password);
 
 }
